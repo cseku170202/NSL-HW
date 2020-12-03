a94d500c50f6bd278fe0aa4958ea4cd868e5ee4e
Md Shymun Islam <43073057+cseku170202@users.noreply.github.com>
2020-12-03 23:04:45 +0600

    Regex

---
+++ Python-basic/hr_Validating_Roman_Numerals.py
@@ -0,0 +1,9 @@
+thousands = '(M{0,3})'
+hundreds = '(CM|CD|D?C{0,3})'
+tens = '(XC|XL|L?X{0,3})'
+digits = '(IX|IV|V?I{0,3})'
+
+regex_pattern = r"%s%s%s%s$" % (thousands, hundreds, tens, digits)
+
+import re
+print(str(bool(re.match(regex_pattern, input()))))
